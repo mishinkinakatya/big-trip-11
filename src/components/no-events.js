cff50ca076290a5bbd25c05356805f69dad0d0c8
mishinkinakatya <mishinkinakatya@gmail.com>
2020-04-19 22:55:47 +0500

    Замена формы редактирования на карточку события. Отрисовка сообщения-заглушки, если событий нет

---
+++ src/components/no-events.js
@@ -0,0 +1,10 @@
+import AbstractComponent from "./abstract-component.js";
+
+const createNoEventstTemplate = () =>
+  `<p class="trip-events__msg">Click New Event to create your first point</p>`;
+
+export default class NoEvents extends AbstractComponent {
+  getTemplate() {
+    return createNoEventstTemplate();
+  }
+}
--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -1,5 +1,6 @@
 import EventOfDayComponent from "../components/event-of-day.js";
 import EventEditComponent from "../components/event-edit.js";
+import NoEventsComponent from "../components/no-events.js";
 import SortComponent from "../components/sort.js";
 import TripDaysComponent from "../components/trip-days.js";
 import {castDateTimeFormat} from "../utils/common.js";
@@ -14,15 +15,26 @@ const renderEvent = (eventListElement, event) => {
     replace(eventOfDayComponent, eventEditComponent);
   };
 
+  const escKeyDownHandler = (evt) => {
+    const isEscKey = evt.key === `Esc` || evt.key === `Escape`;
+
+    if (isEscKey) {
+      replaceEditToEvent();
+      document.removeEventListener(`keydown`, escKeyDownHandler);
+    }
+  };
+
   const eventOfDayComponent = new EventOfDayComponent(event);
   eventOfDayComponent.setEditButtonClickHandler(() => {
     replaceEventToEdit();
+    document.addEventListener(`keydown`, escKeyDownHandler);
   });
 
   const eventEditComponent = new EventEditComponent(event);
   eventEditComponent.setSubmitHandler((evt) => {
     evt.preventDefault();
     replaceEditToEvent();
+    document.removeEventListener(`keydown`, escKeyDownHandler);
   });
 
   render(eventListElement, eventOfDayComponent, RenderPosition.BEFOREEND);
@@ -32,10 +44,18 @@ export default class TripController {
   constructor(container) {
     this._container = container;
 
+    this._noEventsComponent = new NoEventsComponent();
     this._sortComponent = new SortComponent();
   }
 
   render(events) {
+    const isEvents = events.every((event) => event.type && event.destination && event.startDate && event.endDate && event.price);
+
+    if (!isEvents) {
+      render(this._container, this._noEventsComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
     render(this._container, this._sortComponent, RenderPosition.BEFOREEND);
 
     const eventDays = events.map((it) => [`${it.startDate.getFullYear()}-${castDateTimeFormat(it.startDate.getMonth())}-${castDateTimeFormat(it.startDate.getDate())}`].join(`, `));
